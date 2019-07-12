a2234d59b387f0ccecbd920486c0f23615c468df
quangngoc430 <ngoc.bui150019@vnuk.edu.vn>
2019-07-12 11:16:01 +0700

    Bean scope, life cycle

--- spring-demo-one/src/com/ngoc/springdemo/BeanLifeCycleDemoApp.java
+++ spring-demo-one/src/com/ngoc/springdemo/BeanLifeCycleDemoApp.java
@@ -2,22 +2,19 @@ package com.ngoc.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class HelloSpringApp {
-	
+public class BeanLifeCycleDemoApp {
+
 	public static void main(String[] args) {
 		
 		// load the spring configuration file
 		ClassPathXmlApplicationContext context = 
-				new ClassPathXmlApplicationContext("applicationContext.xml");
-						
+				new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
+		
 		// retrieve bean from spring container
 		Coach theCoach = context.getBean("myCoach", Coach.class);
 		
-		// call methods on the bean
-		System.out.println(theCoach.getDailyWorkout());
 		
-		// let's call our new method for fortunes
-		System.out.println(theCoach.getDailyFortune());
+		System.out.println(theCoach.getDailyWorkout());
 		
 		// close the context
 		context.close();
--- spring-demo-one/src/com/ngoc/springdemo/Coach.java
+++ spring-demo-one/src/com/ngoc/springdemo/Coach.java
@@ -3,4 +3,6 @@ package com.ngoc.springdemo;
 public interface Coach {
 	
 	public String getDailyWorkout();
+	
+	public String getDailyFortune();
 }
